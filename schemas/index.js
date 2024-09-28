5f03c173c1f30fea1d2959d724d4991961b03e05
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2024-09-28 15:08:16 +0300

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -2,12 +2,10 @@ const registerSchema = require('./registerSchema');
 const loginSchema = require('./loginSchema');
 const emailSchema = require('./emailSchema');
 const addSchema = require('./contactSchema');
-const updateFavoriteSchema = require('./updateFavoriteSchema');
 
 module.exports = {
   registerSchema,
   loginSchema,
   emailSchema,
   addSchema,
-  updateFavoriteSchema,
 }
