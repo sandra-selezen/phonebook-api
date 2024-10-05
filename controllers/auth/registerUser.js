f43b7913c515eeeec8753c4ae635324fc00a9546
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2024-10-05 15:03:06 +0300

    Update registerUser.js

--- controllers/auth/registerUser.js
+++ controllers/auth/registerUser.js
@@ -7,7 +7,7 @@ const { User } = require('../../models');
 const { HttpError } = require('../../helpers');
 
 const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
@@ -15,14 +15,19 @@ const registerUser = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const payload = {
-    name,
-    email,
 
-  }
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  const userId = newUser._id;
+
+  const payload = {
+    id: userId,
+  };
+
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
 
-  const newUser = await User.create({ ...req.body, password: hashPassword, token })
+  newUser.token = token;
+  await newUser.save();
 
   res.status(201).json({
     token,
